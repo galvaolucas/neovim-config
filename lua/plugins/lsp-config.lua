d0fd6c936e82b51cca4927fc34629a6ace4efbac
Lucas Galvao <lucas.galvao@instacasa.com.br>
2024-06-13 13:19:58 -0300

    adicionando tokyonight

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,8 +1,18 @@
 return {
   {
     "williamboman/mason.nvim",
+    dependencies = {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
     config = function()
       require("mason").setup()
+      local mason_tool_installer = require("mason-tool-installer")
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "prettier", -- prettier formatter
+          "stylua", -- lua formatter
+        },
+      })
     end,
   },
   {
@@ -11,6 +21,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
+          "cssls",
           "eslint",
           "html",
           "tsserver",
@@ -26,10 +37,20 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
+      lspconfig.eslint.setup({
+        settings = {
+          useFlatConfig = true,
+          experimental = {
+            useFlatConfig = true,
+          },
+        },
+      })
       lspconfig.html.setup({})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
     end,
   },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
   "nvim-lualine/lualine.nvim",
   config = function()
     options = {
-      theme = "dracula",
+      theme = "tokyonight",
     }
   end,
 }
